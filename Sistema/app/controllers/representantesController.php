d859f07f027d9c350aa0a3c2e38b442389e842da
Alexander De Azevedo <alexander_deazevedo@hotmail.com>
2018-02-16 11:47:21 -0400

    Ya se puede Insertar a la base de datos

--- Sistema/app/controllers/representantesController.php
+++ Sistema/app/controllers/representantesController.php
@@ -15,46 +15,35 @@ use app\models as Model;
 use Ocrend\Kernel\Router\IRouter;
 use Ocrend\Kernel\Controllers\Controllers;
 use Ocrend\Kernel\Controllers\IControllers;
-
+  
 /**
- * Controlador representantes/
+ * Controlador ninos/
  *
- * @author Alexander De Azevedo, Sergio García y Greg Gómez <oeneikaphotos@gmail.com>
+ * @author Ramon García, Fernando Gomes y Alexander De Azevedo <oeneikaphotos@gmail.com>
 */
-
+  
 class representantesController extends Controllers implements IControllers {
 
     public function __construct(IRouter $router) {
-        parent::__construct($router);
-        $r = new Model\Representantes;
-
-        /*switch($this->method) {
-            case 'eliminar':
-              $r->delete($this->isset_id);
-            break;
-            case 'cedula_representante':
-            echo $this->template->render('representantes/representantes',array(
-              'representantes' => $r->get($this->method,$this->isset_id)
-            ));
-            break;
-            case 'nombre':
-            echo $this->template->render('representantes/representantes',array(
-              'representantes' => $r->get($this->method,$this->isset_id)
-            ));
-            break;
-            case 'sexo':
-            echo $this->template->render('representantes/representantes',array(
-              'representantes' => $r->get($this->method,$this->isset_id)
-            ));
-            break;
-            default:
-              echo $this->template->render('representantes/representantes',array(
-                'representantes' => $r->get()
-              ));
-            break;
-          }
-          */
-        echo $this->template->render('representantes/representantes');
+      parent::__construct($router);  
+      global $config; 
+      $r = new Model\Representantes($router);
+      switch ($this->method) {
+        case 'crear':
+          echo $this->template->render('representantes/crear');
+        break;
+        case 'editar':
+          $t->editar();
+        break;
+        case 'eliminar':
+          # code...
+        break;
+        default:
+          echo $this->template->render('representantes/representantes',array(
+          'representante' => $r->get()
+          ));
+          break;
+      }
+      
     }
-
 }
